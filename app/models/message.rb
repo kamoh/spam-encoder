f4311f900ade020e7106823cb186f9e33ced593b
kamoh <ben.serviss@gmail.com>
2014-10-08 22:14:31 -0400

    Starting to stub out 10 phrases for each character

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,43 +1,83 @@
 class Message < ActiveRecord::Base
 
-  attr_accessor :message
-  attr_reader :message_length_key
+  attr_accessor :message, :message_length_key
 
   def library(input)
     phrase_library = {
-      a: ["ARYA ","bucket "],
-      b: ["loans ","hams "],
-      c: ["enhancement ","owls "],
-      d: ["pills ","putz "],
+      a: ["buy now & forevar ","bucket ","tens ","thousands ","more and ","even more but ","butter is not enough ","for u and ","nineety "," flippy softness "],
+      b: ["loans ","really? But "],
+      c: ["enhancement For U ","owls "],
+      d: ["enormously big ","putz canopy "],
       e: ["sale ","elbows "],
-      f: ["nigeria ","saddle "],
-      g: ["deals ","derf "],
-      h: ["bits ","flurf "],
-      i: ["Canadian ","couches "],
+      f: ["Nigerian ","saddle "],
+      g: ["deals ","pillow table? "],
+      h: ["bits ","Peanutest butter! "],
+      i: ["Canadian ","The most "],
       j: ["swing ","macaroni "],
       k: ["FAST ","ikea "],
       l: ["tremendous ","nokia "],
       m: ["LIMITED ","vizio "],
       n: ["funds ","vayagrao "],
       o: ["~~! ","subway "],
-      p: ["great ","tunnel syndrome "],
+      p: ["grUndly ","tunnel syndrome "],
       q: ["business ","carpal "],
       r: ["opportunity ","tarsal "],
-      s: ["fields ","salad "],
-      t: ["calling ","meats "],
-      u: ["sales ","bacons "],
-      v: ["act ","discounts "],
-      w: ["did ","short time "],
-      x: ["right ","freedom "],
-      y: ["main ","aprons "],
-      z: ["scandal ","conches "],
-      ‡: ["the ","bagel "],
+      s: ["cold tomatoes ","salad "],
+      t: ["police Chief Jim Bravura ","meats "],
+      u: ["times in one night! ","bacons "],
+      v: ["ripped in feet of passion ","discounts "],
+      w: ["shamoon ","short time "],
+      x: ["river danse ","freedom "],
+      y: ["court held appearance ","aprons "],
+      z: ["hell hole of a sasquatch ","conches "],
+      A: ["unbelievable deal ","great offer "],
+      B: ["house mortgage ","*~*~* "],
+      C: ["act immediately ","hurry upp "],
+      D: ["ghost dollars ","clinet Estwood "],
+      E: ["trombone mostly ","Bab Saougat "],
+      F: ["street fighter elevent ","sandwich makeur "],
+      G: ["THE MOON "," fifteen goods "],
+      H: ["marmalade witches ","carpentry bastards "],
+      I: ["forty cravens ","leopard queens "], 
+      J: ["kign of crabs ","great good goblins "],
+      K: ["TODAY TODAY RIGTH AWAY ","god damn lies "],
+      L: ["pour another one ","FRANK MILLAR "],
+      M: ["javelin poppin ","car elevator vroom "],
+      N: ["no man baseball ","bad jimmies "],
+      O: ["right straight away, ","cricket patch "],
+      P: ["SO MUCH ","baseless horoscope "],
+      Q: ["mummified dolphindroid ","eloquent raizers "],
+      R: ["king german ","grumbling soundblaster "],
+      S: ["how can you fart? ","Learn tricks and trips "],
+      T: ["vacation ca TIOn ","sign pitty runnykine "],
+      U: ["trippy tae ","crep on the windowsell "],
+      V: ["rutabager eater ","albrega braintruster "],
+      W: ["Carven imagur ","reddits pajams "],
+      X: ["cartoon skips ","rabbit nightmare "],
+      Y: ["Spike elevation ","film terrorist "],
+      Z: ["sandy bagelry ","lunch bonnet "],
+      "?" => ["collapsable nursey ","daycare CHEEP "],
+      "!" => ["so many, ","great goobly "],
+      "," => ["cheese testicles ","tertiary diarrhea "],
+      "." => ["marvelous, the cans ","castle trying to play "],
+      "'" => ["can't poop tomorrow ","send spam lovely "],
+      ‡: ["the ","bagel ","the ","bagel ","the ","bagel ","the ","bagel ","the ","bagel "],
       "\n" => ["tremmel ","craps "],
       ♤: ["tremmel ","craps "]
     }
     if !input[:letter].nil?
-      return phrase_library[input[:letter].to_sym][message_length_key] if !phrase_library[input[:letter].to_sym].nil?
-      return input[:letter]
+      #binding.pry
+      @message_length_key += 1
+      #binding.pry
+      @message_length_key -= 11 if @message_length_key > 9
+      # message_length_key = (0..9).to_a.sample
+      if !phrase_library[input[:letter].to_sym].nil?
+        return phrase_library[input[:letter].to_sym][@message_length_key]
+      elsif !phrase_library[input[:letter]].nil?
+        return phrase_library[input[:letter]][@message_length_key]
+      else
+        return input[:letter]
+      end
     else
       phrase_library.each { |k,v| 
         v.each do |phrase|
@@ -49,7 +89,8 @@ class Message < ActiveRecord::Base
   end
 
   def encode_message(message)
-    @message_length_key = 1
+    @message_length_key = 0
+    #binding.pry
     # @message_length_key = message.length.to_s[0].to_i
     @message = message
     new_message = []
