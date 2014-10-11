f7a48e4282ae01b81965e88d114087fca2ece944
kamoh <ben.serviss@gmail.com>
2014-10-11 17:33:53 -0400

    Comment out initializer

--- config/application.rb
+++ config/application.rb
@@ -20,18 +20,18 @@ module SpamEncoder
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    initializer 'setup_asset_pipeline', :group => :all do |app|
-      # We don't want the default of everything that isn't js or css, because it pulls too many things in
-      app.config.assets.precompile.shift
-      # Explicitly register the extensions we are interested in compiling
-      app.config.assets.precompile.push(Proc.new do |path|
-        File.extname(path).in? [
-          '.html', '.erb', '.haml', # Templates
-          '.png', '.gif', '.jpg', '.jpeg', # Images
-          '.eot', '.otf', '.svc', '.woff', '.ttf', # Fonts
-        ]
-      end)
-    end
+    # initializer 'setup_asset_pipeline', :group => :all do |app|
+    #   # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    #   app.config.assets.precompile.shift
+    #   # Explicitly register the extensions we are interested in compiling
+    #   app.config.assets.precompile.push(Proc.new do |path|
+    #     File.extname(path).in? [
+    #       '.html', '.erb', '.haml', # Templates
+    #       '.png', '.gif', '.jpg', '.jpeg', # Images
+    #       '.eot', '.otf', '.svc', '.woff', '.ttf', # Fonts
+    #     ]
+    #   end)
+    # end
 
   end
 end
