c5c098f15e35d088e5c6ddade742cb75bc97254e
kamoh <ben.serviss@gmail.com>
2014-10-11 17:11:07 -0400

    Updates config for static assets

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Get Rails to serve assets
-config.serve_static_assets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,6 +12,9 @@ module SpamEncoder
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Get Rails to serve assets
+    config.serve_static_assets = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
