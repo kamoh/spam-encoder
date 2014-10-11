3599cdb2ab0f02e939ca37389078a7717d00a130
kamoh <ben.serviss@gmail.com>
2014-10-11 17:09:46 -0400

    Updates favicon, tries re-adding css and js files to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# Get Rails to serve assets
+config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
